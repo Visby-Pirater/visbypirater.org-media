d04ec3a6ca01fad29dcd8fad8bdabeae8f8f6d4f
fiskhandlarn <oskar@joelson.org>
2021-05-14 13:21:55 +0200

    Add zip script

---
+++ zip.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# https://unix.stackexchange.com/a/86727
+for d in ./media/visbypirater/*; do
+    if [ -d "$d" ]; then
+        mp3dir="$d/mp3/"
+        if [ -d "$mp3dir" ]; then
+            zip -rvDj "$d/$(basename $d).mp3.zip" "$mp3dir"
+        fi
+    fi
+done
